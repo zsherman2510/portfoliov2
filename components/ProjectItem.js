9e86ae88d4d504aa4cbda04d4f1effa7377b75d2
zavion sherman <zsherman2510@hotmail.com>
2022-04-02 18:30:05 -0700

    fixed the card flex properties

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -9,10 +9,10 @@ export default function ProjectItem({ project }) {
   const techSplits = project.tech.split("/");
 
   return (
-    <>
-      <div className={styles.project}>
-        <div className={styles.img}>
-          {/* <Image
+		<>
+			<div className={styles.project}>
+				<div className={styles.img}>
+					{/* <Image
             src={
               project.thumbnail
                 ? project.thumbnail.formats.small.url
@@ -21,49 +21,52 @@ export default function ProjectItem({ project }) {
             width={400}
             height={400}
           /> */}
-          <img
-            src={
-              project.thumbnail
-                ? project.thumbnail.formats.small.url
-                : "/images/default.jpg"
-            }
-            alt="thumbnail"
-          />
-        </div>
+					<img
+						src={
+							project.thumbnail
+								? project.thumbnail.formats.small.url
+								: "/images/default.jpg"
+						}
+						alt="thumbnail"
+					/>
+				</div>
 
-        <div className={styles.project_desc}>
-          <div className={styles.project_tag}>
-            {techSplits.map((tech) => (
-              <div key={tech} className={styles.project_tech}>
-                {tech}
-              </div>
-            ))}
-          </div>
-          <div className={styles.project_title}>{project.name}</div>
+				<div className={styles.project_desc}>
+					<div className={styles.project_tag}>
+						{techSplits.map((tech) => (
+							<div key={tech} className={styles.project_tech}>
+								{tech}
+							</div>
+						))}
+					</div>
+					<div className={styles.project_title}>{project.name}</div>
 
-          <div className={styles.project_info}>{project.description}</div>
-
-          <a
-            href={project.link}
-            className={styles.project_link}
-            target="_blank"
-          >
-            View live demo
-          </a>
-          {user.auth ? (
-            <>
-              <a
-                className={styles.project_link}
-                href={`${NEXT_URL}/projects/edit/${project.id}`}
-              >
-                Edit Project
-              </a>
-            </>
-          ) : (
-            <span></span>
-          )}
-        </div>
-      </div>
-    </>
+					<div className={styles.project_info}>
+						{project.description}
+					</div>
+					<div className={styles.button_group}>
+						<a
+							href={project.link}
+							className={styles.project_link}
+							target="_blank"
+						>
+							View live demo
+						</a>
+						{user.auth ? (
+							<>
+								<a
+									className={styles.project_link}
+									href={`${NEXT_URL}/projects/edit/${project.id}`}
+								>
+									Edit Project
+								</a>
+							</>
+						) : (
+							<span></span>
+						)}
+					</div>
+				</div>
+			</div>
+		</>
   );
 }
